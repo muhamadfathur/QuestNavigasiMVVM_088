826ffe0ff6d05f3b248d80247583c9c84364cd0a
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-11-21 21:20:26 +0700

    membuat fungsi DetailMahasiswaView pada folder model di file DataMahasiswaView.kt

--- app/src/main/java/com/dev/mvvm/ui/view/DataMahasiswaView.kt
+++ app/src/main/java/com/dev/mvvm/ui/view/DataMahasiswaView.kt
@@ -35,25 +35,8 @@ fun DetailMahasiswaView(
                 isiParam = items.second
             )
         }
-        Button(onClick = { onClickButton() }) {
-            Text("Back")
+
         }
     }
 }
 
-@Composable
-fun CardSection(judulParam: String, isiParam: String) {
-    Column {
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = judulParam, modifier = Modifier.weight(0.8f))
-            Text(text = ":", modifier = Modifier.weight(0.2f))
-            Text(
-                text = "$isiParam",
-                modifier = Modifier.weight(2f)
-            )
-        }
-    }
-}
